0f10729b622543ab2d7a4b9cfb905c31b076d612
jahmed0 <j.ahmed99@outlok.com>
2022-04-14 10:01:13 +0000

    activity 4 script

--- activity4/checkPassword.sh
+++ activity4/checkPassword.sh
@@ -2,22 +2,16 @@
 
 #Write a script to validate how secure someone's password is.
 read -r -p "Enter your password: " userPass
-valid=true
 if ! [[ ${#userPass} -ge 8 ]]
 then
-	valid=false
 	echo "Invalid password. Your password must contain more than 8 characters."
 
 elif ! [[ $userPass =~ [0-9] ]]
 then
-	valid=false
 	echo "Invalid password. Your password must contain a number."
-elif ! [[ $userPass =~ [a-z] ]] && ! [[ $userPass =~ [A-Z] ]]
+elif [[ $userPass =~ [a-z] ]] && [[ $userPass =~ [A-Z] ]]
 then
-	valid=false
+	echo "Congratulations! Your password is secure."
+else
 	echo "Invalid password. Your password must contain both uppercase and lowercase  letters."
 fi
-#elif [[ $valid == true ]]
-#	echo echo "Congratulations! Your password is secure."
-#       exit 0
-#fi

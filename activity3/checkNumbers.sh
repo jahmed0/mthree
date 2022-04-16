5a88f6a7d1f046fd8a1e1c9d23471453524f6131
jahmed0 <j.ahmed99@outlok.com>
2022-04-16 15:33:55 +0000

    activity 3 updates

--- activity3/checkNumbers.sh
+++ activity3/checkNumbers.sh
@@ -10,9 +10,15 @@ echo "Check your lottery numbers here.";
 matches=0;
 while true
 do
-	#lineNo=`wc -l userNumbers.log`
-	#if [ "$lineNo"  -le 4 ]
-	#then
+	lineNumber=`wc -l userNumbers.log`;
+	echo $lineNumber
+	if [ -z "$lineNo" ]
+	then
+		lineNumber=0;
+		echo $lineNumber
+	fi
+	if [ "$lineNumber" -le 4 ]
+	then
 		read -p "Please enter a lottery number between 1-50: " input;
 		until [[ "$input" =~ ^[0-9]+$ ]] && [ "$input" -ge 1 ] && [ "$input" -le 50 ] 
 		do
@@ -28,15 +34,15 @@ do
 			echo $input >> userNumbers.log
 			cat userNumbers.log
 		fi
-	#else
-	#	read -p "Please enter a bonus lottery number between 1-10: " bonus;
-	#	until [[ "$bonus" =~ ^[0-9]+$ ]] && [ "$bonus" -ge 1 ] && [ "$bonus" -le 10 ]
-        #       do
-        #                echo "Please enter a valid number.";
-        #                read -p "Please enter a lottery number between 1-10: " bonus;
-        #        done
-	#	echo $input >> userNumbers.log
-	#	break
-	#fi
+	else
+		read -p "Please enter a bonus lottery number between 1-10: " bonus;
+		until [[ "$bonus" =~ ^[0-9]+$ ]] && [ "$bonus" -ge 1 ] && [ "$bonus" -le 10 ]
+               do
+                        echo "Please enter a valid number.";
+                        read -p "Please enter a lottery number between 1-10: " bonus;
+                done
+		echo $input >> userNumbers.log
+		break
+	fi
 done
 
